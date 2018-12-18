0d6fb5690eb820b2b1759f9b6efde22de4b6fdee
pgjones <philip.graham.jones@googlemail.com>
2018-12-18 23:09:52 +0000

    Correct the classifier to not Python 3.7 or better
    (Quart supports 3.7 or better).

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
